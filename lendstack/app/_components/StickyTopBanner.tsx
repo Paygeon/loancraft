cb3cc03f1f17d5d7a26bd8df1a4708a339362321
Vashon Gonzales <paygeoncard@gmail.com>
2024-06-17 18:42:33 +0000

    updated banner

--- lendstack/app/_components/StickyTopBanner.tsx
+++ lendstack/app/_components/StickyTopBanner.tsx
@@ -12,24 +12,10 @@ import Link from 'next/link';
  */
 export default function StickyTopBanner() {
 	return (
-		<div className="w-full bg-neutral-200 text-center text-black space-y-2 py-2 px-12">
-			<p className="text-center leading-6 tracking-wide">
-				This is an example project build in top of the boilerplate template{' '}
-				<Link
-					href="https://lendingstack.com/templates/ui-component-library"
-					className="underline"
-				>
-					<span className="font-semibold">Ui Component Library</span>
-				</Link>{' '}
-				by{' '}
-				<Link href="https://lendingstack.com" className="underline">
-					lendingstack
-				</Link>
-				.
-			</p>
-			<p>
-				What you see is what you get. Meaning, this is exactly what you get when
-				you clone the repository and run the project.
+		<div className="w-full bg-red-200 text-center text-black space-y-2 py-2 px-12">
+			<p className="text-center font-bold leading-6 tracking-wide">
+				We are currently running a "pilot program" and are accepting new users on a rolling basis{' '}
+				<br/>
 			</p>
 		</div>
 	);
